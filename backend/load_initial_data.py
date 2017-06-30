883214a3409e2e1f2f2e68d0037190d8cee44cfe
moshekan <kanoni.moshe@gmail.com>
2017-06-30 06:04:41 +0300

    Fixed DB init problem.

--- backend/load_initial_data.py
+++ backend/load_initial_data.py
@@ -1,37 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from models import *
+import os
 
 
 def load_initial_data():
-    """
-    track1 = Track(name="Up and Enter")
-    track2 = Track(name="XML Masters")
-    track3 = Track(name="Mor's Angels")
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        track1 = Track(name=u"למעלה ואנטר")
+        teacher = Teacher(name=u"ליאור ב.")
+        teacher.track = track1
 
-    teacher = Teacher(name="Lior Bet")
-    teacher.track = track1
 
-    teacher2 = Teacher(name="Itai")
-    teacher2.track = track2
+        track2 = Track(name="Up and Enter")
+        teacher2 = Teacher(name="Itai")
+        teacher2.track = track2
 
-    teacher3 = Teacher(name="Itai")
-    teacher3.track = track3
-
-    db.session.add(teacher2)
-    db.session.add(teacher)
-    db.session.commit()
-    """
-    # track1 = Track(name=u"למעלה ואנטר")
-    # teacher = Teacher(name=u"ליאור ב.")
-    # teacher.track = track1
-    #
-    #
-    # track2 = Track(name="Up and Enter")
-    # teacher2 = Teacher(name="Itai")
-    # teacher2.track = track2
-    #
-    # db.session.add(teacher2)
-    # db.session.add(teacher)
-    # db.session.commit()
-    pass
+        db.session.add(teacher2)
+        db.session.add(teacher)
+        db.session.commit()
