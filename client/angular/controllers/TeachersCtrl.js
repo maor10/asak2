7d2c4de2b7fb8d326554da9759ac04a8bc0dc57c
Maor Kern <maor.kern@gmail.com>
2017-06-30 05:43:25 +0300

    profile done

--- client/angular/controllers/TeachersCtrl.js
+++ client/angular/controllers/TeachersCtrl.js
@@ -1,10 +1,11 @@
 /**
  * Represents the teachers controller
  */
-app.controller('TeachersCtrl', function($scope, Track, Teacher) {
+app.controller('TeachersCtrl', function($scope, $location, Track, Teacher) {
 
     this.tracks = Track.query();
     this.teachers = Teacher.query();
+
 
     /**
      * Select a track to filter by
@@ -13,17 +14,19 @@ app.controller('TeachersCtrl', function($scope, Track, Teacher) {
         this.teachers = this.teachers.filter(function (teacher) {
             return teacher.track_id = this.selectedTrackID;
         });
+        console.log(this.teachers);
     };
 
     /**
      * Gets matching teachers by teacher name
      */
     this.getMatchingTeachers = function(searchText) {
-        console.log("searhing for " + searchText + " in " + JSON.stringify(this.teachers));
+        console.log("searching");
         return this.teachers.filter(function (teacher) {
             return teacher.name.indexOf(searchText) > -1;
         });
     };
+
 });
 
 
