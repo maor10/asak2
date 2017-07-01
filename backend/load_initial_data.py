9813496c55a5e29c2f2494de4b606b7ec0886d6e
moshekan <kanoni.moshe@gmail.com>
2017-07-02 00:35:22 +0300

    Added Data Init

--- backend/load_initial_data.py
+++ backend/load_initial_data.py
@@ -8,20 +8,25 @@ def load_initial_data():
     if os.environ.get("WERKZEUG_RUN_MAIN") == "true" and not Track.query.filter_by(name=u"Up And Enter").first():
         up_enter = Track(name=u"Up And Enter")
         up_enter.teachers = [Teacher(name=u"ליאור ב."), Teacher(name=u"ויאס מלך הוואסח"), Teacher(name=u"אבשלום"),
-                             Teacher(name=u'יובל זיתים'), Teacher(name=u'דריזין'), Teacher(name=u'הליאור האשכנזי')]
+                             Teacher(name=u'יובל זיתים'), Teacher(name=u'דריזין'), Teacher(name=u'הליאור האשכנזי'),
+                             Teacher(name=u"סופרבן")]
 
-        xml_masters = Track(name=u"XML Masters")
-        xml_masters.teachers = [Teacher(name=u"איתי AKA carrot man")]
+        xml_masters = Track(name=u"XML & TPY Masters")
+        xml_masters.teachers = [Teacher(name=u"איתי איש הגזר"), Teacher(name=u"ענבל"), Teacher(name=u"אל(ז)ה"),
+                                Teacher(name=u"מיכל שינה המיספרית"), Teacher(name=u"אילאיל"), Teacher(name=u"אלינוע"),
+                                Teacher(name=u"שלי הטייסת"), Teacher(name=u"אייברי")]
 
-        code_monkeys = Track(name=u"Code Monkeys")
-        code_monkeys.teachers = [Teacher(name=u"אהוד התותח"), Teacher(name=u"שרון"), Teacher(name=u"יונתן")
-            ,Teacher(name=u"מור")]
 
-        boring = Track(name=u"תרגיל המשעמם בייקום")
-        boring.teachers = [Teacher(name=u"שי")]
+        code_monkeys = Track(name=u"המלאכיות של מור")
+        code_monkeys.teachers = [Teacher(name=u"אהוד"), Teacher(name=u"שרון"), Teacher(name=u"יונתן"),
+                                 Teacher(name=u"מור"), Teacher(name=u"אדם"), Teacher(name=u"אילנה"),
+                                 Teacher(name=u"ניצן"), Teacher(name=u"בר"), Teacher(name=u"נתי")]
+
+        boring = Track(name=u"החדשים")
+        boring.teachers = [Teacher(name=u"שי"), Teacher(name=u"בן"), Teacher(name=u"גל"), Teacher(name=u"שחף")]
 
         eyals = Track(name=u"Amit Eyal And Sons")
-        eyals.teachers = []
+        eyals.teachers = [Teacher(name=u"קדם"), Teacher(name=u"סער"), Teacher(name=u"נועם"), Teacher(name=u"שולץ")]
 
         db.session.add(up_enter)
         db.session.add(xml_masters)
